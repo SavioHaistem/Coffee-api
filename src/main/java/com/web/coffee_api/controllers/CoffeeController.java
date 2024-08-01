06985497201ed80d37137911dcac12e91eb403c1
SavioHaistem <savio.dantas.5858@gmail.com>
2024-08-01 08:19:39 -0300

    feat: setting docker compose

--- src/main/java/com/web/coffee_api/controllers/CoffeeController.java
+++ src/main/java/com/web/coffee_api/controllers/CoffeeController.java
@@ -1,4 +1,16 @@
 package com.web.coffee_api.controllers;
 
+import com.web.coffee_api.entities.Coffee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
 public class CoffeeController {
+    @GetMapping
+    public ResponseEntity<Coffee> getById() {
+        return ResponseEntity.ok().body(new Coffee(1L,"lunatic coffee","lunar coffee",1.0,10.0));
+    }
 }
