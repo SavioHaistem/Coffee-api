319311a4973e8cf0eb402b385fda412a31e04275
SavioHaistem <savio.dantas.5858@gmail.com>
2024-08-02 18:06:39 -0300

    feat: configure initial h2 settings

--- src/main/java/com/web/coffee_api/entities/Coffee.java
+++ src/main/java/com/web/coffee_api/entities/Coffee.java
@@ -1,11 +1,20 @@
 package com.web.coffee_api.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "coffees_tb")
 public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
