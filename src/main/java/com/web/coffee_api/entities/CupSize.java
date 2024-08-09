eb9c87f7b58652a5a754efca71ab2622aa07faf1
SavioHaistem <savio.dantas.5858@gmail.com>
2024-08-09 15:03:04 -0300

    feat: CupSize entity

--- src/main/java/com/web/coffee_api/entities/CupSize.java
+++ src/main/java/com/web/coffee_api/entities/CupSize.java
@@ -1,4 +1,24 @@
 package com.web.coffee_api.entities;
 
-public class CupSize {
+import com.web.coffee_api.entities.enums.Size;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cup_size_tb")
+public class CupSize implements Serializable {
+    @Serial
+    private final static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Size size;  
 }
