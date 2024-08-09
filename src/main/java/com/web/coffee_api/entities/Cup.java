8837508a2a042464e7ba9e5adf3c2c73a1529a09
SavioHaistem <savio.dantas.5858@gmail.com>
2024-08-09 16:10:46 -0300

    refactor: cup and cupsize relationship

--- src/main/java/com/web/coffee_api/entities/Cup.java
+++ src/main/java/com/web/coffee_api/entities/Cup.java
@@ -1,11 +1,10 @@
 package com.web.coffee_api.entities;
 
-import com.web.coffee_api.entities.enums.Size;
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,16 +12,22 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "cup_size_tb")
-public class CupSize implements Serializable {
+@Table(name = "cup_tb")
+public class Cup implements Serializable {
     @Serial
-    private final static long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @lombok.NonNull
-    private Size size;
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "cupSize")
+    private String name;
     @lombok.NonNull
-    private List<Cup> cups;
+    private String description;
+    @lombok.NonNull
+    private Coffee coffee;
+    @ManyToOne
+    @JoinColumn(name = "cup_size_id")
+    @lombok.NonNull
+    private CupSize cupSize;
 }
