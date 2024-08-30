ccfe18720f5b7d2bf4b2dace1f121331d755c5b5
SavioHaistem <savio.dantas.5858@gmail.com>
2024-08-30 17:25:29 -0300

    feat: personalized exceptions

--- src/main/java/com/web/coffee_api/services/exceptions/IllegalOperation.java
+++ src/main/java/com/web/coffee_api/services/exceptions/IllegalOperation.java
@@ -2,11 +2,20 @@ package com.web.coffee_api.services.exceptions;
 
 import java.io.Serial;
 
-public class IlegalOperation extends RuntimeException {
+public class IllegalOperation extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
+    private String server_message;
+    public IllegalOperation(String server_message, String error_message) {
+        super(error_message);
+        this.server_message = server_message;
+    }
 
-    public IlegalOperation(String message) {
-        super(message);
+    public String getServer_message() {
+        return server_message;
+    }
+
+    public void setServer_message(String server_message) {
+        this.server_message = server_message;
     }
 }
---
+++ src/main/java/com/web/coffee_api/services/exceptions/ResourceNotFound.java
@@ -0,0 +1,10 @@
+package com.web.coffee_api.services.exceptions;
+
+
+public class ResourceNotFound extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
