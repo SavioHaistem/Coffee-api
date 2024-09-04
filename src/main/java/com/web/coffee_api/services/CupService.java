78ebfbac9c3cbbde6f579973590c3d454d78f1b3
SavioHaistem <savio.dantas.5858@gmail.com>
2024-09-04 17:07:56 -0300

    fix: update cup and associate with updated sizes #2

--- src/main/java/com/web/coffee_api/services/CupService.java
+++ src/main/java/com/web/coffee_api/services/CupService.java
@@ -13,7 +13,9 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CupService implements ServiceBasics<Cup> {
@@ -27,25 +29,42 @@ public class CupService implements ServiceBasics<Cup> {
     }
 
     public Cup findById(Long id) {
-        return cupRepository.findById(id).orElseThrow(()->
+        return cupRepository.findById(id).orElseThrow(() ->
                 new ResourceNotFound("can't find cup at id: " + id)
         );
     }
 
     public Cup insert(Cup cup) {
         if (cupRepository.findById(cup.getId()).isEmpty()) {
-            cup.getSizes().forEach(System.out::println);
-            sizeRepository.saveAll(cup.getSizes());
+            Set<CupSize> validSizes = new HashSet<>();
+            for (CupSize size : cup.getSizes()) {
+                CupSize validSize = sizeRepository.findById(size.getId()).orElseThrow(() ->
+                        new ArgumentsException(
+                        "size at id: "
+                                + size.getId()
+                                + " can't be found",
+                        "caused by insert Cup method at CupService",
+                        HttpStatus.NOT_ACCEPTABLE
+                ));
+
+                cup.getSizes().remove(size);
+                cup.getSizes().add(validSize);
+                validSize.addCup(cup);
+                validSizes.add(validSize);
+            }
+
             Long generatedId = cupRepository.save(cup).getId();
+            sizeRepository.saveAll(validSizes);
             return cupRepository.findById(generatedId).orElseThrow();
+        } else {
+            throw new ArgumentsException(
+                    "cup at id: "
+                            + cup.getId()
+                            + " already exists",
+                    "caused by: insert method at CupService",
+                    HttpStatus.NOT_FOUND
+            );
         }
-        throw new ArgumentsException(
-                "cup at id: "
-                + cup.getId()
-                + " already exists",
-                "caused by: insert method at CupService",
-                HttpStatus.CONFLICT
-        );
     }
 
     public Cup updateById(Long id, Cup new_cup) {
@@ -55,11 +74,26 @@ public class CupService implements ServiceBasics<Cup> {
         old_cup.getCoffeeCups().removeAll(old_cup.getCoffeeCups());
         old_cup.getCoffeeCups().addAll(new_cup.getCoffeeCups());
         old_cup.getSizes().removeAll(old_cup.getSizes());
-        old_cup.getSizes().addAll(new_cup.getSizes());
+        
+        for (CupSize size : new_cup.getSizes()) {
+            CupSize validSize = sizeRepository.findById(size.getId()).orElseThrow(()->
+                    new ArgumentsException(
+                    "cannot associate this size at id: "
+                            + size.getId()
+                            + " because the size can't be found",
+                    "caused by update Cup by id method at CupService",
+                    HttpStatus.NOT_FOUND
+            ));
+
+            old_cup.getSizes().add(validSize);
+            validSize.getCups().add(old_cup);
+        }
+
+        sizeRepository.saveAll(old_cup.getSizes());
         Long generatedId = cupRepository.save(old_cup).getId();
 
-        return cupRepository.findById(generatedId).orElseThrow(()->
-            new ResourceNotFound("can't find updated cup at id: " + generatedId)
+        return cupRepository.findById(generatedId).orElseThrow(() ->
+                new ResourceNotFound("can't find updated cup at id: " + generatedId)
         );
     }
 
@@ -70,7 +104,7 @@ public class CupService implements ServiceBasics<Cup> {
             throw new IllegalOperation("can't remove cup at id: "
                     + id
                     + " because this is associated with others entities"
-                    ,e.getMessage());
+                    , e.getMessage());
         }
     }
 }
